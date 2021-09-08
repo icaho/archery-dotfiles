db4d5902db2027b9e95e8472ec30553efc13798c
Matt Thorpe <mjthorpe@elitemail.org>
2021-09-08 10:47:06 +0100

    updating ssh alias because alacritty

--- .oh-my-zsh/custom/alias.zsh
+++ .oh-my-zsh/custom/alias.zsh
@@ -1,4 +1,5 @@
 alias nexmo-prod='aws-gen-creds --profile nexmo-prod -a arn:aws:iam::920763156836:role/vgr-admin'
 alias nexmo-dev='aws-gen-creds --profile nexmo-dev -a arn:aws:iam::564623767830:role/vgr-admin'
 alias vi='nvim'
+alias ssh='TERM=xterm ssh'
 export EDITOR=/usr/bin/nvim
