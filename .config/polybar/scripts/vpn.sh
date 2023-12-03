7aa2b37c8ffac9806a6a2ebda343f0e5011b1f32
Matt <mjthorpe@elitemail.org>
2023-12-03 00:28:22 +0000

    updating dotfiles

--- .config/polybar/scripts/vpn.sh
+++ .config/polybar/scripts/vpn.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 VPN=`nmcli c show --active | grep vpn | awk '{print $1}' | sed -e "s/^/< /; s/*$//; s/$/ >/"`
+TUN=`nmcli c show --active | grep tun | awk '{print $1}' | sed -e "s/^/< /; s/*$//; s/$/ >/"`
 
-echo -e $VPN
+
+if [ -z "$VPN" ] && [ -n "$TUN" ]; then
+    echo -e "AWS Client VPN"
+else
+    echo -e $VPN
+fi
 
